4cea19b77fbc729ac98ad56fb5068ed9a21c7c52
Mark Thorne <thorne.mark@gmail.com>
2022-03-26 23:58:52 -0500

    Tried practicing some string extraction; needs more work!

--- 5-string_operations.sh
+++ 5-string_operations.sh
@@ -3,5 +3,15 @@
 
 motto="Each man made his own shipwreck."
 echo Our motto has ${#motto} characters.
-expr index "$motto" "h"		# 4; count starts at 1 (not 0)
+expr index "$motto" "h"		# 4; with 'expr' the count starts at 1 (not 0)
 expr length "$motto"
+
+# Practicing string extraction
+name1="praenomen=Marcus,nomen=Tullius,cognomen=Cicero"
+P_END=$(expr index $name1 ',')
+COMMA1=$(expr index $name1 '=')
+P_LENGTH=$((P_END - COMMA1))
+echo First equals sign is at index $COMMA1.
+echo First comma is at index $P_END.
+PRAE=$(name1:$COMMA1:$P_LENGTH)
+echo His praenomen is $PRAE.
