6395808844b3819910c0cd917390eeee385fe5da
Mark Thorne <thorne.mark@gmail.com>
2022-03-27 23:58:04 -0500

    Expanded string extraction to include full name

--- 5-string_operations.sh
+++ 5-string_operations.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/bash
 # Some practice with basic string operations
 
 motto="Each man made his own shipwreck."
@@ -8,10 +8,32 @@ expr length "$motto"
 
 # Practicing string extraction
 name1="praenomen=Marcus,nomen=Tullius,cognomen=Cicero"
-P_END=$(expr index $name1 ',')
-COMMA1=$(expr index $name1 '=')
-P_LENGTH=$((P_END - COMMA1))
-echo First equals sign is at index $COMMA1.
-echo First comma is at index $P_END.
-PRAE=$(name1:$COMMA1:$P_LENGTH)
-echo His praenomen is $PRAE.
+
+# Extracting the praenomen
+COMMA1=$(expr index $name1 ',')
+EQU1=$(expr index $name1 '=')
+PRAE_LENGTH=$((COMMA1 - EQU1 - 1))
+
+echo First equals sign is at index $EQU1.
+echo First comma is at index $COMMA1.
+echo The praenomen length is $PRAE_LENGTH characters.
+PRAENOMEN=${name1:$EQU1:$PRAE_LENGTH}
+
+# Extracting the nomen
+REM_1=${name1:$COMMA1}
+COMMA2=$(expr index $REM_1 ',')
+EQU2=$(expr index $REM_1 '=')
+NOM_LENGTH=$((COMMA2 - EQU2 - 1))
+NOMEN=${REM_1:$EQU2:$NOM_LENGTH}
+
+# Extracting the cognomen
+REM_2=${REM_1:$COMMA2}
+EQU3=$(expr index $REM_2 '=')
+COGNOMEN=${name1:$EQU2}
+
+
+
+
+echo "Cicero's full Latin name was: $PRAENOMEN $NOMEN $COGNOMEN."
+
+
