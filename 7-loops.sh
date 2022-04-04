4898171d99e81d0148c18ce9af8b27e2992a02df
Mark Thorne <thorne.mark@gmail.com>
2022-04-03 23:01:10 -0500

    Put the read command inside the while loop and solved the multiple arguments error

--- 7-loops.sh
+++ 7-loops.sh
@@ -3,19 +3,32 @@
 
 POETS=(Homer Vergil Ovid Lucan)
 
+# Loop through the POETS array
 for p in ${POETS[@]}; do
 	echo "Maybe you'd like to read $p?"
 done
 
-echo "Which poet do you choose?"
-read poet
+# While statement until Lucan is chosen
+poet="not Lucan"
+while [ "$poet" != "Lucan" ]; do
+	echo "Which poet do you choose?"
+	read chosen_poet
+	echo ${#chosen_poet[@]}
+	poet="$chosen_poet"
+	if [[ "$poet" != "Lucan" ]]; then
+		echo "You have chosen one of the greats! But not the greatest..."
+	else 
+		echo "Excellent choice! Lucan is the best, obviously."
+	fi
+done
 
-if [ $poet = "Homer" -o $poet = "Vergil" ]; then
-	echo "You have chosen one of the greats!"
-fi
-
-num_poets=4
+# While statement counting down from 4
+num_poets=${#POETS[@]}
 while [ $num_poets -gt 0 ]; do
-	echo "There are currently $num_poets poets left to read."
+	if [ $num_poets -eq 1 ]; then
+		echo "There is currently $num_poets poet left to read."
+	else
+		echo "There are currently $num_poets poets left to read."
+	fi
 	num_poets=$(($num_poets - 1))
 done
