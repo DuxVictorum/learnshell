924f52e9dfc3729927c1d23d201c0321e5235670
Mark Thorne <thorne.mark@gmail.com>
2022-04-07 22:52:27 -0500

    Improved array comparison from initial algorithm

--- 8-array_comparison.sh
+++ 8-array_comparison.sh
@@ -4,25 +4,14 @@ a=(4 7 28 53)
 b=(3 9 14 28 52 53)
 c=(10 28 46 53 99)
 
-#comparison of first two arrays a and b
-for x in "${a[@]}"; do
-  in=false
-  for y in "${b[@]}"; do
-    if [ $x = $y ];then
-      # assigning the matching results to new array z
-      z[${#z[@]}]=$x
-    fi
-  done
+x=()
+echo ${x[@]}
+for na in ${a[@]}; do
+	for nb in ${b[@]}; do
+		if [ $na -eq $nb ]; then
+			x=(${x[@]} $na)
+		fi
+	done
 done
-#comparison of third array c with new array z
-for i in "${c[@]}"; do
-  in=false
-  for k in "${z[@]}"; do
-    if [ $i = $k ];then
-      # assigning the matching results to new array j
-      j[${#j[@]}]=$i
-    fi
-  done
-done
-# print content of array j
-echo ${j[@]}
+
+echo ${x[@]}
