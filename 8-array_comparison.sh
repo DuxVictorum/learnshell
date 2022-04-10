c26ad0e82af9a9aab22c9e85ac78dca12319a03a
Mark Thorne <thorne.mark@gmail.com>
2022-04-09 22:58:34 -0500

    Finished work on 8-array_comparison.sh

--- 8-array_comparison.sh
+++ 8-array_comparison.sh
@@ -2,11 +2,12 @@
 
 a=(4 7 28 53)
 b=(3 9 14 28 52 53)
-c=(10 28 46 53 99)
+c=(10 29 46 53 99)
 
 # Initialize empty array to hold the shared numbers
 x=()
 y=()
+z=()
 
 # Compare arrays a and b, storing shared numbers in array x
 for na in ${a[@]}; do
@@ -18,12 +19,24 @@ for na in ${a[@]}; do
 done
 echo ${x[@]}
 
-# Compare array a with array c
+# Compare array a with array c, storing shared numbers in array y
 for na in ${a[@]}; do
-        for nc in ${x[@]}; do
+        for nc in ${c[@]}; do
                 if [ $na -eq $nc ]; then
                         y=(${y[@]} $nc)
                 fi
         done
 done
 echo ${y[@]}
+
+# Compare array x and array y, storing shared numbers in array z
+for nx in ${x[@]}; do
+	for ny in ${y[@]}; do
+		if [ $nx -eq $ny ]; then
+			z=(${z[@]} $nx)
+		fi
+	done
+done
+
+# Print out numbers common to all arrays
+echo "The number(s) shared in common across all arrays are/is: ${z[@]}."
