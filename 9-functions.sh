9af1579c5a2d0471d6cc54fdbcc5e68d108d030f
Mark Thorne <thorne.mark@gmail.com>
2022-04-11 23:59:12 -0500

    Started work on ENGLISH_CALC function

--- 9-functions.sh
+++ 9-functions.sh
@@ -14,3 +14,17 @@ echo "The square of $num is $(squareNum)."
 # a) inside the double parentheses of arithmetic expansion you do not need to say $num but just num
 # b) if you declare 'num=$1' *inside* the function, then $1 isn't the first parameter from the bash shell but rather the first parameter from wherever you call the function within the script (in this case line 11).
 # c) Bash isn't great for doing arithmetic compared to modern programming languages, ha. :-) 
+
+
+# Exercise 2 - Function to translate sentences to arithmatic expressions
+
+function ENGLISH_CALC {
+	num1=
+	case $2 in 
+		"plus") echo "$1 + $3 = $(())"
+}
+
+# testing code
+ENGLISH_CALC 3 plus 5
+ENGLISH_CALC 5 minus 1
+ENGLISH_CALC 4 times 6
