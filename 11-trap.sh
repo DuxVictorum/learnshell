c9a1ddc5b0b24bb47dafce8e7f13973594259453
Mark Thorne <thorne.mark@gmail.com>
2022-04-16 22:14:27 -0500

    Worked more on trap commands

--- 11-trap.sh
+++ 11-trap.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/bash
 
-trap "echo 'Dude, bruh!'" SIGINT SIGTERM
+trap "brotime" SIGINT
 
+num_int=0
 num=0
 
-while true; do
-	echo "Time to sleep..."
-	sleep 5
-	num+=5
-done
+brotime() {
+	num_int=$((num_int + 1))
+	if [ $num_int -eq 1 ]; then
+		echo "Type CTRL-C one more time to exit."
+	else
+		echo "Exiting..."
+		exit
+	fi
+}
 
+while [ $num -lt 15 ]; do
+	echo "Time to sleep...$num seconds."
+	sleep 5
+	num=$((num + 5))
+done
