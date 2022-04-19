550671e21fdadf8b55520c3528a7097c53f67657
Mark Thorne <thorne.mark@gmail.com>
2022-04-19 00:16:26 -0500

    Fixed bugs in 12b script

--- 12b-testapalooza.sh
+++ 12b-testapalooza.sh
@@ -5,27 +5,47 @@
 echo "Hello. Enter the name of a file to scan: "
 read file_input
 
-file_status=0
-
 if [ -z $file_input ]; then
 	echo "No file name given. Please re-run command with file name."
+	break
 fi
+
+type=""
 
 if [ -n $file_input ]; then
 	echo "Scanning... "
+	sleep 2
 
+	# Check whether input is regular file, directory, or something else
 	if [ -f $file_input ]; then
 		echo "This is a regular file."
-	else if [ -d $file_input ] ; then
+		type="file"
+	elif [ -d $file_input ] ; then
 		echo "This is a directory, not a file."
+		type="directory"
 	else
 		echo "This is a weird file. I'd recommend burning it in a pile of rubbish. Or ignoring. Your call!"
+		type="weird, crazy thing"
 	fi
 	
 	# Check file/directory ownership
+	echo "Checking ownership..."
+	sleep 2
+
 	if [ -O $file_input ]; then
-		echo "Good news - you ($USER) already own this file!"
+		echo "Good news - you ($USER) already own this $type!"
 	else
-		echo "You are not the owner of this file."
+		echo "You are not the owner of this $type."
 	fi
+
+	# Check whether file or directory is empty
+	echo "Checking contents... "
+	sleep 2
+
+	if [ -s $file_input ] ; then
+		echo "The $type is not empty. Congratulations!"
+	else
+		echo "This is a lonely, empty $type. So desolate."
+	fi
+
 fi
