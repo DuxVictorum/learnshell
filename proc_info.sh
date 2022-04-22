bceed61c752b2687123a7357fafdf5995023d1ac
Mark Thorne <thorne.mark@gmail.com>
2022-04-21 23:56:19 -0500

    More work on named process option; bugs remain!

--- proc_info.sh
+++ proc_info.sh
@@ -10,13 +10,25 @@ if [ -z $1 ] ; then
 	break
 
 # Check whether argument was a (numeric) PID or (alphabetic) process name
+
+# A) If a PID is passed...
 elif [[ $1 =~ ^[0-9]+$ ]] ; then
 	proc_name=$(cat /proc/$1/status | grep Name | awk '{print $2}')
 	num_threads=$(cat /proc/$1/status | grep Threads | awk '{print $2}')
 	echo -e "PID $1 \t$proc_name \tNumber of threads: $num_threads"
+
+# B) Else if a process name string is passed...
 else
-	proc_num=$(ps -eF | grep -m 1 $1 | awk '{print $2}')
-	proc_name=$(cat /proc/$proc_num/status | grep Name | awk '{print $2}')
+	proc_array=(`ps -eF | grep $1 | awk '{print $2}'`)
+	echo ${proc_array[@]}
+	for ps_num in $proc_array[@] ; do
+		ps_name=$(cat /proc/$ps_num/status | grep Name | awk '{print $2}')
+		if [ $ps_name == *"$1"* ] ; then
+			proc_num=$ps_num
+			proc_name=$ps_name
+			break
+		fi
+	done
 	num_threads=$(cat /proc/$proc_num/status | grep Threads | awk '{print $2}')
 	echo -e "PID $proc_num \t$proc_name \tNumber of threads: $num_threads"
 
