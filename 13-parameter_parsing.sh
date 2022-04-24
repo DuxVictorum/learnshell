50392875dd6b14f0af6b038d390b678101f34802
Mark Thorne <thorne.mark@gmail.com>
2022-04-24 00:16:39 -0500

    Further practice with getopts

--- 13-parameter_parsing.sh
+++ 13-parameter_parsing.sh
@@ -2,13 +2,15 @@
 
 # Practice with command line argument parsing and the getopt command
 
-while getopts u:a:f: flag ; do
+while getopts u:a:f:h flag ; do
 	case "${flag}" in
 		u) username=${OPTARG};;
 		a) age=${OPTARG};;
 		f) fullname=${OPTARG};;
+		h) echo "You need help? You're in it to win it, buster! No help for you!"
 	esac
 done
 echo "Username: $username";
 echo "Age: $age";
 echo "Full name: $fullname";
+
