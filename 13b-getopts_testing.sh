c669c1df0618f3463896944023d2aabaedb0e29c
Mark Thorne <thorne.mark@gmail.com>
2022-04-25 23:47:06 -0500

    Fixed help option bug

--- 13b-getopts_testing.sh
+++ 13b-getopts_testing.sh
@@ -2,16 +2,17 @@
 
 # Practice with getopts (following the model of 13-parameter_parsing.sh
 
-while getopts h:p:oh flag ; do
+while getopts h:p:o? flag ; do
 	case $flag in 
 		h) historian=$OPTARG;;
 		p) poet=$OPTARG;;
 		o) echo "Orators are good at everything! But we don't need one today...";;
-		h) echo "You want help, sure, everyone does! But it will cost you. How much you got?";;
+		?) echo "You want help, sure, everyone does! But it will cost you. How much you got?";;
 	esac
 done
 
-if [ -n $historian ] ; then echo "The famous historian $historian is writing about you!" ; fi 
-if [ -n $poet ] ; then echo "The famous poet $poet will be composing a ballad about you!" ; fi
+
+if [ $historian ] ; then echo "The famous historian $historian is writing about you!" ; fi 
+if [ $poet ] ; then echo "The famous poet $poet will be composing a ballad about you!" ; fi
 
 
