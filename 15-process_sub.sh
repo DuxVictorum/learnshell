750ed32e8a956ef6175688b75bca71fa8019b252
Mark Thorne <thorne.mark@gmail.com>
2022-04-29 23:57:42 -0500

    Fixed basics of 15-process_sub.sh

--- 15-process_sub.sh
+++ 15-process_sub.sh
@@ -2,20 +2,26 @@
 
 # Practice with process substitution
 
+files=()
 if [ -d ~/foof ] ; then
-	for f in ~/foof/file?.txt ; do
-		echo "Scanning file ..."
+	cd ~/foof
+	num_files=$(ls file?.txt | wc -l)
+	echo "This directory has $num_files for use. Oh boy!"
+
+	for f in file?.txt ; do
+		echo -n "Scanning file ..."
 		if [ -s $f ] ; then
 			echo "File $f already has contents."
 		else
-			echo "File $f is empty -- but not for long!"
-			echo "Adding contents..."
-			sleep 1
-			echo "These are some words! Enjoy them!" >> $f
+			echo -n "File $f is currently empty." 
 		fi
+		echo -e "\tAdding new contents..."
+		sleep 1
+		num_lines=$(cat $f | wc -l)
+		echo "Now this file has $num_lines lines of text." >> $f
 	done
 else 
 	mkdir ~/foof
-	echo "New directory ~/foof is now made."
+	echo "New directory ~/foof is now made. Please re-run this script."
 fi
 
