6991a8ab05369f05c6c2f95e4ba34493d2bf5ba6
Mark Thorne <thorne.mark@gmail.com>
2022-05-03 23:45:21 -0500

    More basic grep practice

--- 16-grep1.sh
+++ 16-grep1.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/bash
 
-# Grep practice - part #1
+# Grep practice
+gp=grep_practice.txt
+
+# 0 - a quick test of the emergency grep-cast system!
 
 echo "This is not a test
 this is the Real Thing!" | grep -i thin
+
+# 1 - Basic grep exercises
+
+grep 'x[0-9]\{4\}' $gp
+grep '^[0-9]\{3\} ' $gp
+grep ' 2013$' $gp
+
