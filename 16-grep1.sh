f934d8c7e8c7826f7ba5b9337d6d124e38ce36e7
Mark Thorne <thorne.mark@gmail.com>
2022-05-06 09:55:38 -0500

    Finished basic grep exercises

--- 16-grep1.sh
+++ 16-grep1.sh
@@ -2,6 +2,7 @@
 
 # Grep practice
 gp=grep_practice.txt
+GREP_OPTIONS='--color=auto'
 
 # 0 - a quick test of the emergency grep-cast system!
 
@@ -12,27 +13,26 @@ this is the Real Thing!" | grep -i thin
 echo "#1) All lines with a x???? phone extension"
 grep 'x[0-9]\{4\}' $gp
 
-echo "#2) All lines that begin with 3 digits followed by a blank"
+echo -e "\n#2) All lines that begin with 3 digits followed by a blank"
 grep '^[0-9]\{3\} ' $gp
 
-echo "#3) All lines that contain a date"
+echo -e "\n#3) All lines that contain a date"
 grep ' 2013$' $gp
 
-echo "#4) All lines containing a vowel followed by a single character followed by the SAME vowel
-echo -e "\tStill figuring this one out.
-# I can't figure this one out yet: vowel+.+same_vowel [NOT grep '[aeiou].[aeiou]' $gp]
+echo -e "\n#4) All lines containing a vowel followed by a single character followed by the SAME vowel"
+grep '\([aeiou]\).\1' $gp
 
-echo "#5) All lines that do not begin with a capital S :-)"
+echo -e "\n#5) All lines that do not begin with a capital S :-)"
 grep '^[^S]' $gp
 
-echo "#6) All lines that contain CA in upper or lowercase"
+echo -e "\n#6) All lines that contain CA in upper or lowercase"
 grep -i 'ca' $gp
 
-echo "#7) All lines with an email address along with line number"
+echo -e "\n#7) All lines with an email address along with line number"
 grep -n @ $gp
 
-echo "#8) All lines that do NOT contain Sep."
+echo -e "\n#8) All lines that do NOT contain Sep."
 grep -v Sep. $gp
 
-echo "#9) All lines that contain 'de' as a separate word"
-grep grep -w de $gp
+echo -e "\n#9) All lines that contain 'de' as a separate word"
+grep --color=auto -w de $gp
