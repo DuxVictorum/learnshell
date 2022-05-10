0767fde0e4b79ada309bbad7cd8e85bd5dc60831
Mark Thorne <thorne.mark@gmail.com>
2022-05-09 23:59:25 -0500

    More fun with grep colors

--- 16-grep2_colors.sh
+++ 16-grep2_colors.sh
@@ -4,7 +4,9 @@
 # --> Excellent explanation of grep colors: 
 # https://askubuntu.com/questions/1042234/modifying-the-color-of-grep
 
-export GREP_COLORS='ms=04;35'
+# Default value of GREP_COLORS='ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36'
+
+export GREP_COLORS='ms=05;33'
 cpu='/proc/cpuinfo'
 
 grep proc $cpu
