94abf475ee6d83aaa01c417ecff018a9f0deea14
Mark Thorne <thorne.mark@gmail.com>
2022-05-11 23:56:20 -0500

    Fun with grep background colors!

--- 16-grep2_colors.sh
+++ 16-grep2_colors.sh
@@ -6,7 +6,7 @@
 
 # Default value of GREP_COLORS='ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36'
 
-export GREP_COLORS='ms=05;33:mc=01;31:sl=01;35:cx=03;31'
+export GREP_COLORS='ms=05;33;107:sl=01;31;44:cx=09;36'
 cpu='/proc/cpuinfo'
 
 grep -C2 proc $cpu
