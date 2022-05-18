4fd93cd66e55ead9699384e32a3b2eef6ea00f8b
Mark Thorne <thorne.mark@gmail.com>
2022-05-17 23:59:38 -0500

    Yet more fun with grep colors

--- 17-tee.sh
+++ 17-tee.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -i
 
 # Practice with the tee command
 
 gp=grep_practice.txt
  
 cat $gp | grep -i '^oct' | tee gp_results.txt
+
+export GREP_COLORS='ms=38;5;162:sl=38;5;220'
+grep '8' gp_results.txt
