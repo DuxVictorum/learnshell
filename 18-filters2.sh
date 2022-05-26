479cc2fff3352cfd0fd8594b1249e5d509d9f2a0
Mark Thorne <thorne.mark@gmail.com>
2022-05-25 22:58:48 -0500

    Fun with comm and process substitution

--- 18-filters2.sh
+++ 18-filters2.sh
@@ -4,3 +4,9 @@
 
 # 1) Fun with uniq
 echo -e "Your computer has the following processor: $(cat /proc/cpuinfo | grep -oP 'model name.*\: \K.*' | uniq)."
+
+# 2) Fun with comm - I've set up two files with slighly differing phone numbers to compare
+echo -e "\nNow let's compare two sets of phone numbers."
+comm <(sort comm_practice1.txt) <(sort comm_practice2.txt)
+echo -e "\nThe phone numbers common to both sets are as follows:"
+comm -12 <(sort comm_practice1.txt) <(sort comm_practice2.txt)
