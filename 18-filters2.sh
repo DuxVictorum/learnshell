399931414c42c390d732020c3572d65b3916a27e
Mark Thorne <thorne.mark@gmail.com>
2022-05-26 23:43:02 -0500

    Fun with od

--- 18-filters2.sh
+++ 18-filters2.sh
@@ -7,6 +7,10 @@ echo -e "Your computer has the following processor: $(cat /proc/cpuinfo | grep -
 
 # 2) Fun with comm - I've set up two files with slighly differing phone numbers to compare
 echo -e "\nNow let's compare two sets of phone numbers."
-comm <(sort comm_practice1.txt) <(sort comm_practice2.txt)
+comm <(sort comm_practice1.txt) <(sort comm_practice2.txt)   # process substitution!
 echo -e "\nThe phone numbers common to both sets are as follows:"
 comm -12 <(sort comm_practice1.txt) <(sort comm_practice2.txt)
+
+# 3) Fun with od - 
+echo -e "\nThe 'od' command will convert ascii text into octal (or other) format."
+od -bc gp_results.txt
