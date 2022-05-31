e22a5f1338fa694ae7a96036db0d8da55d9b3046
Mark Thorne <thorne.mark@gmail.com>
2022-05-30 23:09:59 -0500

    More fun with sed!

--- 19-filters3.sh
+++ 19-filters3.sh
@@ -2,9 +2,16 @@
 
 # Fun with the 'sed' command
 # 1) Printing every third line of a file
-echo -e "The first use of 'sed' will print every third line, starting with line 1.\n"
+echo -e "The first use of 'sed' will start at line 1 and print every third line.\n"
 sed -n 1~3p grep_practice.txt
 
 # 2) Basic text string replacement
 echo -e "\n\nThe 'sed' command has basic text replacement capabilities. Here 'High School' becomes:"
 grep 'High School' grep_practice.txt | sed 's/High School/Dolphin Puddle/'
+
+# 3) Using sed to delete selection from text stream
+echo -e "\nNow the 'sed' command will delete all the email addresses from 'grep_practice.txt' file!"
+echo -e "\nFirst here are the email addresses in the file."
+grep 'example.com' grep_practice.txt
+echo -e "\n But now the emails are gone!"
+grep 'example.com' grep_practice.txt | sed /example.com/d
